6c1d1dda4b78046d36bcd4c98f948dc7881375c9
karmaxx2024 <karmaxx2023@gmail.com>
2026-05-02 17:52:16 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,56 @@
-# размер окна
-SCREEN_W = 1024
-SCREEN_H = 768
-
-# FPS
+#размер тайла
+TILE_SIZE = 32
+#размер карты
+MAP_WIDTH = 20
+MAP_HEIGHT = 20
+#размер окна
+SCREEN_W = TILE_SIZE * MAP_WIDTH
+SCREEN_H = TILE_SIZE * MAP_HEIGHT
+#FPS
 FPS = 60
-
 # цвета
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GOLD = (255, 215, 0)
+# 🔹 Базовые / Основные
+BLACK       = (0, 0, 0)
+WHITE       = (255, 255, 255)
+RED         = (255, 0, 0)
+GREEN       = (0, 255, 0)
+BLUE        = (0, 0, 255)
+YELLOW      = (255, 255, 0)
+CYAN        = (0, 255, 255)
+MAGENTA     = (255, 0, 255)
+ORANGE      = (255, 165, 0)
+PURPLE      = (128, 0, 128)
+
+# 🖥️ UI и интерфейс
+UI_DARK_BG  = (30, 30, 30)
+UI_LIGHT_BG = (240, 240, 240)
+UI_ACCENT   = (0, 122, 204)
+UI_SUCCESS  = (34, 197, 94)
+UI_WARNING  = (234, 179, 8)
+UI_ERROR    = (239, 68, 68)
+UI_TEXT     = (20, 20, 20)
+UI_TEXT_LG  = (245, 245, 245)
+
+# 🌿 Окружение и природа
+GRASS       = (34, 139, 34)
+SKY         = (135, 206, 235)
+DIRT        = (139, 90, 43)
+WATER       = (0, 105, 148)
+STONE       = (112, 128, 144)
+SAND        = (194, 178, 128)
+FOREST_DARK = (0, 76, 0)
+SNOW        = (255, 250, 250)
+
+# ✨ Магия / Эффекты / Статусы
+FIRE        = (255, 69, 0)
+ICE         = (173, 216, 230)
+POISON      = (127, 255, 0)
+VOID        = (75, 0, 130)
+GOLD        = (255, 215, 0)
+NECRO       = (0, 255, 127)
+BLOOD       = (139, 0, 0)
+AURA        = (100, 149, 237)
+SHIELD      = (176, 196, 222)
